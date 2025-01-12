092f0fbe062a083a5bcd14fd44ea44f82203bcc0
Fred Frank U <fteduche@gmail.com>
2025-01-13 00:05:43 +0100

    last edit

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,11 +1,11 @@
-// Get the current date and time
-const currentTime = new Date();
-
-// Format the date and time
-const formattedTime = currentTime.toLocaleString();
-
-// Get the footer element
-const lastModifiedElement = document.getElementById('last-modified');
-
-// Update the footer text with the current date and time
-lastModifiedElement.textContent = `Last Modification: ${formattedTime}`;
+document.addEventListener('DOMContentLoaded', function() {
+    // Get the current date and time
+    const currentTime = new Date();
+    // Format the date and time
+    const formattedTime = currentTime.toLocaleString();
+    // Get the footer element
+    const lastModifiedElement = document.getElementById('last-modified');
+    // Update the footer text with the current date and time
+    lastModifiedElement.textContent = `Last Modification: ${formattedTime}`;
+  });
+  
